1db310d44f33ab86169ae2b821ed45f4c397eeb5
ANJOEJAMES <anjoejames36@gmail.com>
2026-05-19 13:11:19 +0530

    Test simplest function

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,24 +1,7 @@
-const serverless = require('serverless-http');
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-app.get('/api/health', (req, res) => {
-    res.json({ status: 'ok', message: 'Test API working' });
-});
-
-app.get('/api/test', (req, res) => {
-    res.json({ 
-        env: {
-            turso: !!process.env.TURSO_DB_URL,
-            cloudinary: !!process.env.CLOUDINARY_CLOUD_NAME,
-            jwt: !!process.env.JWT_SECRET
-        }
-    });
-});
-
-module.exports = { app };
-
-exports.handler = serverless(app);
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ status: "ok", message: "Function works!" })
+    };
+};
